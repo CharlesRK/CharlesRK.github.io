be40bfa296f35642f41bdacacff60f6961c0ed25
Amit Kulkarni <amitkulkarni181@gmail.com>
2017-11-02 16:38:28 -0400

    Fixed error

--- script.js
+++ script.js
@@ -6,4 +6,4 @@ var h=window.innerHeight;
 confirm("Height: " + h + "width: " + w);
 
 //Add this in later
-/* If browser width is less than 810 pixels
+/* If browser width is less than 810 pixels */
