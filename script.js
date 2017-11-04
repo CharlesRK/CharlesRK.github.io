0dcd04ad3fa7992586fc3acb8c6589b444c37753
Amit Kulkarni <amitkulkarni181@gmail.com>
2017-11-04 18:10:12 -0400

    Chanigng to :active

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 "use strict";
 
-function hoverOff() {
+/*function hoverOff() {
     var el = this;
     var par = el.parentNode;
     var next = el.nextSibling;
     par.removeChild(el);
     setTimeout(function() {par.insertBefore(el, next);}, 0)
-}
+} */
