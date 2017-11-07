7fd5aa6bae047bbb120c1b9e0b7f69b327ce69db
Amit Kulkarni <amitkulkarni181@gmail.com>
2017-11-07 18:51:50 -0500

    Added experimental typed.js

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 "use strict";
 
+var typed = function() {
+
+  var options = {
+    strings: ["learn", "get jobs", "make a living"],
+    typeSpeed: 150,
+    startDelay: 500,
+    backSpeed: 80,
+    backDelay: 3000,
+    loop: true
+  };
+//  var type = new Typed("#typed", options);
+  $("#typed").typed(options);
+};
+$(document).ready(typed);
+
+
 /*function hoverOff() {
     var el = this;
     var par = el.parentNode;
